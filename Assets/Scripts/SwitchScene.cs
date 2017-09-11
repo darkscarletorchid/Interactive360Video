75f18e70561f7f9f5e601d5a13793d98f102ffb3
Anastasia Trepytion <dark.scarlet.orhid@gmail.com>
2017-09-11 21:22:37 +0300

    Initial commit

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour {
+
+	public string sceneName;
+
+	void Start () {
+
+	}
+
+	public void LoadNextScene() {
+		//SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+		Application.LoadLevel(sceneName);
+
+	}
+}
