3ee16a80dd58dc58b1944b9ebfb050198fd5d2e1
Anastasia Trepytion <dark.scarlet.orhid@gmail.com>
2017-09-17 15:32:58 +0300

    everything work in editor, problems with rendering on android

--- Assets/Scripts/PlayVideo.cs
+++ Assets/Scripts/PlayVideo.cs
@@ -5,26 +5,47 @@ using UnityEngine.Video;
 
 public class PlayVideo : MonoBehaviour, TimedInputHandler {
 
-	public GameObject gameObject;
-	private VideoPlayer _videoPlayer;
+	public GameObject gObject;
+	public Sprite mySprite;
+	public GameObject playBtn;
+	public GameObject switchScenes;
 
-	// Use this for initialization
-	void Start () {
-		_videoPlayer = gameObject.GetComponent(typeof(VideoPlayer)) as VideoPlayer;
-	}
+	private VideoPlayer _videoPlayer;
+	private MeshRenderer _meshRenderer;
+
 		
-	public void PlayAlphaVideo()
-	{
-		Debug.Log (_videoPlayer.clip);
-		_videoPlayer.Play();
+	void Start () {
+		_videoPlayer = gObject.GetComponent(typeof(VideoPlayer)) as VideoPlayer;
+		_videoPlayer.Prepare ();
+		gObject.GetComponent<MeshRenderer>().enabled = false;
+
+		playBtn = GameObject.Find ("Play");
 	}
+
+	private IEnumerator coroutine;
 
 	public void  HandleTimedInput()
 	{
-		Debug.Log (_videoPlayer.clip + "timed");
-		_videoPlayer.Play();
-		var playBtn = GameObject.Find ("Play");
-		playBtn.SetActive (false);
+		if (_videoPlayer.isPrepared) {
+			gObject.GetComponent<MeshRenderer>().enabled = true;
+			_videoPlayer.Play ();
 
+			playBtn.SetActive (false);
+			switchScenes.SetActive (false);
+
+			//on video stop event delegate
+			_videoPlayer.loopPointReached += EndReached;
+		}
+	}
+		
+	void EndReached(VideoPlayer vp)
+	{
+		playBtn.SetActive(true);
+		switchScenes.SetActive (true);
+
+		gObject.GetComponent<MeshRenderer>().enabled = false;
+
+		var spr = playBtn.GetComponent<SpriteRenderer>();
+		spr.sprite = mySprite;
 	}
 }
